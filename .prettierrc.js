47c073a953b3be284170b9764db32ad49374743b
Tom Davidson <tom@tomdavidson.org>
2021-03-20 02:00:19 -0600

    Add initial editor helps

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  singleQuote: true,
+
+  // default
+  trailingComma: 'es5',
+
+  // .editorconfig overrides, but max_line_length deprecated is being debated, so keep it here:
+  // https://github.com/editorconfig/editorconfig/issues/387
+  printWidth: 100,
+
+  // .editorconfig overrides
+  tabWidth: 2,
+  useTabs: false,
+
+  // defined in .editorconfig and .gitattrs
+  endOfLine: 'lf',
+};
